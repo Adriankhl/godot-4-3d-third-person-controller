4e68672b0354ef72f47783a6457eed1a14472727
Adriankhl <adrian.k.h.lai@outlook.com>
2024-05-11 20:02:28 +0800

    feat: add llava ui

--- LLM/llava.gd
+++ LLM/llava.gd
@@ -4,14 +4,48 @@ signal llava_visiable
 signal llava_invisible
 
 var _original_mouse_mode: Input.MouseMode = Input.MOUSE_MODE_CAPTURED
+var image: Image = Image.new()
+
 
 func _physics_process(_delta):
-	if (!$LlavaUI.visible and Input.is_action_just_pressed("open_llm")):
-		$LlavaUI.visible = true
+	if (!$InputUI.visible and Input.is_action_just_pressed("open_llm")):
+		$InputUI.visible = true
+		$OutputUI.visible = true
+		image = get_viewport().get_texture().get_image()
+		var texture = ImageTexture.create_from_image(image)
+		$InputUI/Screen.texture = texture
 		_original_mouse_mode = Input.mouse_mode
 		emit_signal("llava_visiable")
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if($LlavaUI.visible and Input.is_action_just_pressed("pause")):
-		$LlavaUI.visible = false
+	if($InputUI.visible and Input.is_action_just_pressed("pause")):
+		$InputUI.visible = false
+		$OutputUI.visible = false
 		emit_signal("llava_invisible")
 		Input.mouse_mode = _original_mouse_mode
+	
+	if (Input.is_action_just_pressed("toggle_mouse_capture")):
+		if (Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _on_generate_button_pressed():
+	$OutputUI/Output.text = ""
+	$InputUI/GenerateButton.disabled = true
+	run_generate_text_image($InputUI/Prompt.text, image)
+
+
+func _on_generate_text_finished(text):
+	$InputUI/GenerateButton.disabled = false
+	$OutputUI/CloseButton.disabled = false
+
+
+func _on_generate_text_updated(new_text):
+	$OutputUI.visible = true
+	$OutputUI/CloseButton.disabled = true
+	$OutputUI/Output.text += new_text
+
+
+func _on_close_button_pressed():
+	$OutputUI.visible = false
